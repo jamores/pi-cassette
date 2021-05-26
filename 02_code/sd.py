6e4fba85c38324ef692e62b22cb000ed5e079a81
Jose Amores <biomol@gmail.com>
2021-05-26 16:17:04 +0200

    SD SPI id

--- 02_code/sd.py
+++ 02_code/sd.py
@@ -5,9 +5,12 @@ import sdcard
 import spi_handler
 import uos
 
+SD_SPI_ID = 0
+
 def run():
-    spi = SPI(0, sck=pinout.SPI_SCK, mosi=pinout.SPI_MOSI, miso=pinout.SPI_MISO)
-    spih = spi_handler.SPIHandler(0,spi,pinout.SPI_CS_SD)
+    spi = SPI(SD_SPI_ID, sck=pinout.SPI_SCK, mosi=pinout.SPI_MOSI, miso=pinout.SPI_MISO)
+
+    spih = spi_handler.SPIHandler(SD_SPI_ID,spi,pinout.SPI_CS_SD)
     sd = sdcard.SDCard(spi, pinout.SPI_CS_SD)
     uos.mount(sd, '/sd')
 
